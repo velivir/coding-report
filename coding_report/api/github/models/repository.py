8b962f429d4508310a2183b6a78ae140b001098a
velivir <vitaliyantonoff@gmail.com>
2020-09-01 14:08:21 +0600

    refactor(github): transfer base user model to base.py file

--- coding_report/api/github/models/repository.py
+++ coding_report/api/github/models/repository.py
@@ -2,26 +2,11 @@ from typing import Optional
 
 from pydantic import BaseModel
 
+from coding_report.api.github.models.base import BaseUser
 
-class Owner(BaseModel):
-    login: str
-    id: int
-    node_id: str
-    avatar_url: str
-    gravatar_id: Optional[str]
-    url: str
-    html_url: str
-    followers_url: str
-    following_url: str
-    gists_url: str
-    starred_url: str
-    subscriptions_url: str
-    organizations_url: str
-    repos_url: str
-    events_url: str
-    received_events_url: str
-    type: str
-    site_admin: bool
+
+class Owner(BaseUser):
+    pass
 
 
 class Permissions(BaseModel):
