67880f9c8de4d5ec810c40aa7816691ad6712ed6
velivir <vitaliyantonoff@gmail.com>
2020-09-01 12:32:29 +0600

    refactor(github): make explicit data transfer
    for authorization on Github

--- coding_report/api/github/github.py
+++ coding_report/api/github/github.py
@@ -3,23 +3,22 @@ from typing import Any, Mapping, Optional
 from requests import get
 from requests.auth import HTTPBasicAuth
 
-from coding_report.api.github.settings import get_settings
 
-settings = get_settings()
-
-GITHUB_LOGIN = settings.github_login
-GITHUB_API_TOKEN = settings.github_token
-
-
-def fetch_data_from_github(relative_url: str) -> Optional[Mapping[str, Any]]:  # type: ignore
+def fetch_data_from_github(  # type: ignore
+    github_login: str, github_api_token: str, relative_url: str,
+) -> Optional[Mapping[str, Any]]:
     """Fetch data from github."""
     raw_response = get(
         f'https://api.github.com{relative_url}',
-        auth=HTTPBasicAuth(GITHUB_LOGIN, GITHUB_API_TOKEN),
+        auth=HTTPBasicAuth(github_login, github_api_token),
     )
     return raw_response.json() if raw_response else None
 
 
-def fetch_repos_list() -> Optional[Mapping[str, Any]]:  # type: ignore
+def fetch_repos_list(github_login: str, github_api_token: str) -> Optional[Mapping[str, Any]]:  # type: ignore
     """Fetch repositories list from github owner."""
-    return fetch_data_from_github(relative_url=f'/users/{GITHUB_LOGIN}/repos')
+    return fetch_data_from_github(
+        github_login=github_login,
+        github_api_token=github_api_token,
+        relative_url=f'/users/{github_login}/repos',
+    )
