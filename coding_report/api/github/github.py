19ed852cf0688b93f50e03a82e25404c1c04ba3b
velivir <vitaliyantonoff@gmail.com>
2020-09-02 12:31:35 +0600

    refactor(github): move variables into constants
    github_login, owner and github_api_token

--- coding_report/api/github/github.py
+++ coding_report/api/github/github.py
@@ -3,40 +3,33 @@ from typing import Any, Mapping, Optional
 from requests import get
 from requests.auth import HTTPBasicAuth
 
+from coding_report.api.github.settings import get_settings
 
-def fetch_data_from_github(  # type: ignore
-    github_login: str, github_api_token: str, relative_url: str,
-) -> Optional[Mapping[str, Any]]:
+settings = get_settings()
+GITHUB_LOGIN = settings.github_login
+GITHUB_API_TOKEN = settings.github_token
+REPO_OWNER = settings.repo_owner
+
+
+def fetch_data_from_github(relative_url: str) -> Optional[Mapping[str, Any]]:  # type: ignore
     """Fetch data from github."""
     raw_response = get(
         f'https://api.github.com{relative_url}',
-        auth=HTTPBasicAuth(github_login, github_api_token),
+        auth=HTTPBasicAuth(GITHUB_LOGIN, GITHUB_API_TOKEN),
     )
     return raw_response.json() if raw_response else None
 
 
-def fetch_repos_list(github_login: str, github_api_token: str) -> Optional[Mapping[str, Any]]:  # type: ignore
+def fetch_repos_list() -> Optional[Mapping[str, Any]]:  # type: ignore
     """Fetch repositories list from github owner."""
-    return fetch_data_from_github(
-        github_login=github_login,
-        github_api_token=github_api_token,
-        relative_url=f'/users/{github_login}/repos',
-    )
+    return fetch_data_from_github(relative_url=f'/users/{REPO_OWNER}/repos')
 
 
-def fetch_repo_info(github_login: str, github_api_token: str, owner: str, repo_name: str):
+def fetch_repo_info(repo_name: str):
     """Fetches detailed information about a repository."""
-    return fetch_data_from_github(
-        github_login=github_login,
-        github_api_token=github_api_token,
-        relative_url=f'/repos/{owner}/{repo_name}',
-    )
+    return fetch_data_from_github(relative_url=f'/repos/{REPO_OWNER}/{repo_name}')
 
 
-def fetch_commits(github_login: str, github_api_token: str, owner: str, repo_name: str):
+def fetch_commits(repo_name: str):
     """Fetches detailed information about repository commits."""
-    return fetch_data_from_github(
-        github_login=github_login,
-        github_api_token=github_api_token,
-        relative_url=f'/repos/{owner}/{repo_name}/commits',
-    )
+    return fetch_data_from_github(relative_url=f'/repos/{REPO_OWNER}/{repo_name}/commits')
