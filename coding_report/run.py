1075c9352b4e7fb3177d448416513eb5b512521e
velivir <vitaliyantonoff@gmail.com>
2020-09-02 12:55:07 +0600

    feat(main): add getting commits made for today

--- coding_report/run.py
+++ coding_report/run.py
@@ -4,7 +4,8 @@ from typing import Generator
 import pytz
 from dateutil import parser
 
-from coding_report.api.github.github import fetch_repos_list
+from coding_report.api.github.github import fetch_commits, fetch_repos_list
+from coding_report.api.github.models.commit import Commit
 from coding_report.api.github.models.repository import Repository
 from coding_report.settings import get_settings
 
@@ -12,7 +13,8 @@ from coding_report.settings import get_settings
 def main() -> None:
     """Entry point."""
     raw_repos_content = fetch_repos_list()
-    get_repository_names_updated_today(raw_repos_content)
+    repository_names_updated_today = get_repository_names_updated_today(raw_repos_content)
+    get_commits_for_today(repository_names_updated_today)
 
 
 def is_updated_today(last_push_time: str) -> bool:
@@ -34,6 +36,16 @@ def get_repository_names_updated_today(raw_repos_content) -> Generator[str, None
         Repository(**raw_repo_content).name
         for raw_repo_content in raw_repos_content
         if is_updated_today(Repository(**raw_repo_content).pushed_at)
+    )
+
+
+def get_commits_for_today(repository_names_updated_today: Generator[str, None, None]) -> Generator[Commit, None, None]:
+    """Gets the commits made today."""
+    return (
+        Commit(**raw_commit)
+        for repo_name in repository_names_updated_today
+        for raw_commit in fetch_commits(repo_name)
+        if is_updated_today(Commit(**raw_commit).commit.author.date)
     )
 
 
