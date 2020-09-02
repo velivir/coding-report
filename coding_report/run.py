31bf31db3a9f5a39bb1a506468b5d189c9d1a1c6
velivir <vitaliyantonoff@gmail.com>
2020-09-02 14:10:46 +0600

    feat(main): add statistics collection of repositories updated today

--- coding_report/run.py
+++ coding_report/run.py
@@ -1,5 +1,6 @@
 import datetime
-from typing import Generator
+from dataclasses import dataclass
+from typing import Generator, List
 
 import pytz
 from dateutil import parser
@@ -10,11 +11,20 @@ from coding_report.api.github.models.repository import Repository
 from coding_report.settings import get_settings
 
 
+@dataclass
+class RepositoryStatistics:  # noqa: WPS306
+    """Repository statistics data."""
+
+    name: str
+    url: str
+    commits: List[str]
+
+
 def main() -> None:
     """Entry point."""
     raw_repos_content = fetch_repos_list()
-    repository_names_updated_today = get_repository_names_updated_today(raw_repos_content)
-    get_commits_for_today(repository_names_updated_today)
+    repositories_updated_today = get_repositories_updated_today(raw_repos_content)
+    collect_coding_statistics_for_today(repositories_updated_today)
 
 
 def is_updated_today(last_push_time: str) -> bool:
@@ -30,23 +40,41 @@ def is_updated_today(last_push_time: str) -> bool:
     return midnight < local_time_of_last_update_of_repo < now
 
 
-def get_repository_names_updated_today(raw_repos_content) -> Generator[str, None, None]:
+def get_repositories_updated_today(raw_repos_content) -> Generator[Repository, None, None]:
     """Get a list of repositories that are pushed to today."""
     return (
-        Repository(**raw_repo_content).name
+        Repository(**raw_repo_content)
         for raw_repo_content in raw_repos_content
         if is_updated_today(Repository(**raw_repo_content).pushed_at)
     )
 
 
-def get_commits_for_today(repository_names_updated_today: Generator[str, None, None]) -> Generator[Commit, None, None]:
+def get_commits_for_today(repository_name: str) -> List[Commit]:
     """Gets the commits made today."""
-    return (
+    commits_for_today = [
         Commit(**raw_commit)
-        for repo_name in repository_names_updated_today
-        for raw_commit in fetch_commits(repo_name)
+        for raw_commit in fetch_commits(repository_name)
         if is_updated_today(Commit(**raw_commit).commit.author.date)
-    )
+    ]
+    commits_for_today.reverse()
+    return commits_for_today
+
+
+def collect_coding_statistics_for_today(
+    repositories_updated_today: Generator[Repository, None, None],
+) -> List[RepositoryStatistics]:
+    """Collects commit names across all repositories updated today."""
+    return [
+        RepositoryStatistics(
+            name=repository_updated_today.name,
+            url=repository_updated_today.url,
+            commits=[
+                commit.commit.message.split('\n\n')[0]
+                for commit in get_commits_for_today(repository_updated_today.name)
+            ],
+        )
+        for repository_updated_today in repositories_updated_today
+    ]
 
 
 if __name__ == '__main__':
