aa59fa00de64dde12b1515a6833aff25185a67cf
velivir <vitaliyantonoff@gmail.com>
2020-09-04 11:40:07 +0600

    refactor(main): move generation and sending of messages to separate file

---
+++ coding_report/message.py
@@ -0,0 +1,64 @@
+from typing import List
+
+import apprise
+
+from coding_report.statistics.github_statistics import RepositoryStatistics
+
+
+def generate_message_with_github_statistics(github_statistics) -> List[str]:
+    """Generates a message with github statistics."""
+    message = []
+    commits_msg: List[str] = []
+    for repository_statistics in github_statistics:
+        commits_msg.clear()
+        for commit_text in repository_statistics.commits:
+            commits_msg.append(
+                f'\n 🔹 {commit_text}'.format(
+                    commit_text=commit_text,
+                ),
+            )
+        message.append(
+            'Github\n\nРепозиторий - {repository_name}\nКоммитов - {commit_count}: \n{commit_names}\n\n'.format(
+                repository_name=repository_statistics.name,
+                commit_count=len(commits_msg),
+                commit_names=''.join(commits_msg),
+            ),
+        )
+    return message
+
+
+def generate_message_with_wakatime_statistics(wakatime_statistics: float) -> str:
+    """Generates a message with wakatime statistics."""
+    return '{splitter}\nWakatime - {duration}'.format(
+        splitter=10 * '-',
+        duration=convert_seconds_to_hours(wakatime_statistics),
+    )
+
+
+def create_report_message(
+    github_statistics: List[RepositoryStatistics], coding_duration_by_wakatime: float,
+) -> List[str]:
+    """Creates a message for sending statistics to Telegram."""
+    message_with_github_statistics = generate_message_with_github_statistics(github_statistics)
+    message_with_wakatime_statistics = generate_message_with_wakatime_statistics(coding_duration_by_wakatime)
+    return [
+        ''.join(message_with_github_statistics),
+        message_with_wakatime_statistics if message_with_wakatime_statistics else '',
+    ]
+
+
+def send_report_message_to_telegram(telegram_token: str, report_message: str, date: str) -> None:
+    """Sends coding statistics to Telegram."""
+    apobj = apprise.Apprise()
+    apobj.add(f'tgram://{telegram_token}/')
+    apobj.notify(
+        body=report_message,
+        title=f'Статистика кодинга за {date}',
+    )
+
+
+def convert_seconds_to_hours(seconds: float) -> str:
+    """Converts seconds to hours and minutes."""
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return '%dч:%02dм:%02dс' % (hours, minutes, seconds)
--- coding_report/run.py
+++ coding_report/run.py
@@ -1,13 +1,13 @@
 import datetime
-from typing import List
-
-import apprise
 
 from coding_report.api.github.github import fetch_repos_list
 from coding_report.api.wakatime.wakatime import fetch_durations_from_wakatime
+from coding_report.message import (
+    create_report_message,
+    send_report_message_to_telegram,
+)
 from coding_report.settings import get_settings
 from coding_report.statistics.github_statistics import (
-    RepositoryStatistics,
     collect_coding_statistics_for_today,
     get_repositories_updated_today,
 )
@@ -22,61 +22,16 @@ def main() -> None:  # noqa: WPS210
     raw_repos_content = fetch_repos_list()
     repositories_updated_today = get_repositories_updated_today(raw_repos_content)
     github_statistics = collect_coding_statistics_for_today(repositories_updated_today)
-    wakatime_statistics = calculate_coding_duration(
+    coding_duration_by_wakatime = calculate_coding_duration(
         fetch_durations_from_wakatime(
             date=datetime.datetime.today().strftime('%Y-%m-%d'),
         ),
     )
-    report_message = create_report_message(github_statistics, wakatime_statistics)
+    report_message = ''.join(create_report_message(github_statistics, coding_duration_by_wakatime))
     send_report_message_to_telegram(
         telegram_token=settings.telegram_token,
         report_message=report_message,
         date=str(datetime.datetime.today().date()),
-    )
-
-
-def convert_seconds_to_hours(seconds: float) -> str:
-    """Converts seconds to hours and minutes."""
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    return '%dч:%02dм:%02dс' % (hours, minutes, seconds)
-
-
-def create_report_message(github_statistics: List[RepositoryStatistics], wakatime_statistics: float) -> str:
-    """Creates a message for sending statistics to Telegram."""
-    message = []
-    commits_msg: List[str] = []
-    for repository_statistics in github_statistics:
-        commits_msg.clear()
-        for commit_text in repository_statistics.commits:
-            commits_msg.append(
-                f'\n 🔹 {commit_text}'.format(
-                    commit_text=commit_text,
-                ),
-            )
-        message.append(
-            'Github\n\nРепозиторий - {repository_name}\nКоммитов - {commit_count}: \n{commit_names}\n\n'.format(
-                repository_name=repository_statistics.name,
-                commit_count=len(commits_msg),
-                commit_names=''.join(commits_msg),
-            ),
-        )
-    message.append(
-        '{splitter}\nWakatime - {duration}'.format(
-            splitter=10 * '-',
-            duration=convert_seconds_to_hours(wakatime_statistics),
-        ),
-    )
-    return ''.join(message)
-
-
-def send_report_message_to_telegram(telegram_token: str, report_message: str, date: str) -> None:
-    """Sends coding statistics to Telegram."""
-    apobj = apprise.Apprise()
-    apobj.add(f'tgram://{telegram_token}/')
-    apobj.notify(
-        body=report_message,
-        title=f'Статистика кодинга за {date}',
     )
 
 
